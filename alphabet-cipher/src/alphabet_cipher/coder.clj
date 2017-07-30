43c6767e7b640360be3ee237c680fa3ba7ee7c38
mattiasthalen <mattias.thalen@gmail.com>
2017-07-30 11:00:03 +0200

    Update coder.clj

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,68 +1,68 @@
 (require '[clojure.string :as str])
 (ns alphabet-cipher.coder)
 
-;convert the char to int
-(defn char-to-int [c]
-  ;convert to int, then subtract 97 to move from ascii range to 0-25
-  (- (int c) 97))
-
-;convert int to char
-(defn int-to-char [i]
-  ;add 97 to int to move to ascii range, then convert it to char
-  (char (+ i 97)))
-
 ;define encode function to encode the message using a keyword
-(defn encode [keyword message]
-  (let [ ;repeat keyword
+(defn encode [keyword message & [decode]]
+  (let [ ;repeat keyword to be at least as long as the message
          repeated-keyword (apply str (repeat (Math/ceil (/ (count message) (count keyword))) keyword))
+        
+         ;convert string to list of ints
+         str-to-ints (fn [s]
+                       (->>
+                         ;make string a list of chars
+                         (seq  s)
+                         ;map chars to ints
+                         (map int ,,,)
+                         ;subtract 97 from ints to set the range to 0-25
+                         (map #(- % 97) ,,,)))
+         
          ;convert repeated-keyword to ints
-         keyword-int (map char-to-int (seq repeated-keyword))
+         keyword-int (str-to-ints repeated-keyword)
+        
          ;convert message to ints
-         message-int (map char-to-int (seq message))]
+         message-int (str-to-ints message)]
     
-    ;leverage ints against each other and spit out the encoded message
+    ;leverage ints against each other and spit out the message
     (->>
-      ;sum message- int & keyword-int
-      (map + message-int keyword-int)
-      ;mod the result
+      ;check if decode mode is selected
+      (if decode
+        ;if decoding, subtract keyword-int from message-int
+        (map - message-int keyword-int)
+        
+        ;if not decoding, sum message-int & keyword-int
+        (map + message-int keyword-int))
+     
+      ;mod the result so z+1=a
       (map #(mod % 26) ,,,)
-      ;convert ints to string
-      (map int-to-char ,,,)
+     
+      ;add 97 to ints to go back to ascii range
+      (map #(+ % 97) ,,,)
+     
+      ;convert ints to chars
+      (map char ,,,)
+     
       ;make string from list
       (apply str ,,,))))
 
 ;define decode function to decode the message using a keyword
 (defn decode [keyword message]
-  (let [ ;repeat keyword
-         repeated-keyword (apply str (repeat (Math/ceil (/ (count message) (count keyword))) keyword))
-         ;convert repeated-keyword to ints
-         keyword-int (map char-to-int (seq repeated-keyword))
-         ;convert message to ints
-         message-int (map char-to-int (seq message))]
-    
-    ;leverage ints against each other and spit out the decoded message
-    (->>
-      ;subtract message-int by keyword-int
-      (map - message-int keyword-int)
-      ;mod the result
-      (map #(mod % 26) ,,,)
-      ;convert ints to string
-      (map int-to-char ,,,)
-      ;make string from list
-      (apply str ,,,))))
+  ;call encode function with decode set to true
+  (encode keyword message true))
 
 ;define decipher funtion to find the keyword used for encoding
 (defn decipher [cipher message]
   (let [ ;decode to keyword string
-         key-string (decode message cipher)]
+         key-str (decode message cipher)]
     
-    ;iterate through key-string to find keyword
+    ;iterate through substrings of key-str to find keyword
     (loop [i 1]
-      ;set keyword from 0 to current iteration
-      (let [keyword (subs key-string 0 i)]
+      ;set keyword to substring of key-str from 0 to current iteration
+      (let [keyword (subs key-str 0 i)]
+        
       ;check if encoding message with current keyword results in the correct cipher
       (if (= (encode keyword message) cipher)
         ;return keyword if the correct keyword is found
         keyword
+        
         ;if not correct keyword, increase next iteration
         (recur (inc i)))))))
