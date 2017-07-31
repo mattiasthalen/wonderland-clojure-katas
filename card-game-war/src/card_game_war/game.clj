963da9e61a6075bf1098d2645c294d813a8005b2
mattias.thalen <mattias.thalen@gmail.com>
2017-07-31 15:14:32 -0400

    message

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -1,3 +1,4 @@
+(require '[clojure.string :as str])
 (ns card-game-war.game)
 
 ;; feel free to use these cards or use your own data structure
@@ -8,6 +9,92 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+;shuffled deck
+(def shuffled-deck (seq (shuffle cards)))
 
-(defn play-game [player1-cards player2-cards])
+;generate player1 hand
+(def player1-cards
+  (take-nth 2 shuffled-deck))
+
+;generate player2 hand
+(def player2-cards
+  ;return cards filtered by cards not in player1's hand
+  (filter (fn [x] (not (contains? (set player1-cards) x))) shuffled-deck))
+
+;which hand wins?
+(defn play-round [player1-card player2-card]
+  (let [ ;get rank of player 1's card value
+         p1-val-rank (.indexOf ranks (get player1-card 1))
+         ;get rank of player 2's card value
+         p2-val-rank (.indexOf ranks (get player2-card 1))
+         ;get rank of player 1's card suit
+         p1-suit-rank (.indexOf suits (get player1-card 0))
+         ;get rank of player 2's card suit
+         p2-suit-rank (.indexOf suits (get player2-card 0))]
+    ;Return winner
+    (cond
+      ;if p1-val rank is higher, p1 wins
+      (> p1-val-rank p2-val-rank) player1-card
+      ;if p2-val rank is higher, p2 wins
+      (< p1-val-rank p2-val-rank) player2-card
+      ;if none is higher, check the suits
+      :else (cond
+              ;if p1-suit rank is higher, p1 wins
+              (> p1-suit-rank p2-suit-rank) player1-card
+               ;if p2-suit rank is higher, p2 wins
+              (< p1-suit-rank p2-suit-rank) player2-card
+              :else "same card!"))))
+
+;play the game!
+(defn play-game [player1-cards player2-cards]
+  (let [ ;setup p1 hand
+         p1-hand (atom player1-cards)
+         
+         ;setup p2 hand
+         p2-hand (atom player2-cards)]
+    
+    ;iterate through hands while both still have cards
+    (while (every? false? [(empty? @p1-hand) (empty? @p2-hand)])
+      (if
+        ;check if player 1 won the round
+        (= (play-round (last @p1-hand) (last @p2-hand)) (last @p1-hand))
+        
+        ;player 1 gets cards
+        (do
+          ;add cards to player 1's hand
+          (reset! p1-hand
+                  (->
+                    ;get all cards but the last one
+                    (butlast @p1-hand)
+                    
+                    ;add the winning card
+                    (conj ,,, (last @p1-hand))
+                    
+                    ;add the loosing card
+                    (conj ,,, (last @p2-hand))))
+
+          ;remove card from player 2's hand
+          (swap! p2-hand drop-last))
+        
+        ;else player 2 gets cards
+        (do
+          ;add cards to player 2's hand
+          (reset! p2-hand
+                  (->
+                    ;get all cards but the last one
+                    (butlast @p2-hand)
+                    
+                    ;add the winning card
+                    (conj ,,, (last @p2-hand))
+                    
+                    ;add the loosing card
+                    (conj ,,, (last @p1-hand))))
+          
+          ;remove card from player 1's hand
+          (swap! p1-hand drop-last))))
+        
+    ;announce winner
+    (if
+      (empty? @p2-hand)
+      "Player 1"
+      "Player 2")))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -5,15 +5,22 @@
 
 ;; fill in  tests for your game
 (deftest test-play-round
-  (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
+  (testing "the highest rank wins the cards in the round")
+    (is (= [:spade 10] (play-round [:spade 10] [:club 2])))
   (testing "queens are higher rank than jacks")
+    (is (= [:spade :queen] (play-round [:spade :queen] [:club :jack])))
   (testing "kings are higher rank than queens")
+    (is (= [:spade :king] (play-round [:spade :king] [:club :queen])))
   (testing "aces are higher rank than kings")
+    (is (= [:spade :ace] (play-round [:spade :ace] [:club :king])))
   (testing "if the ranks are equal, clubs beat spades")
+    (is (= [:club :ace] (play-round [:spade :ace] [:club :ace])))
   (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (= [:diamond :ace] (play-round [:diamond :ace] [:club :ace])))
+  (testing "if the ranks are equal, hearts beat diamonds")
+    (is (= [:heart :ace] (play-round [:diamond :ace] [:heart :ace]))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
-
+  (testing "the player loses when they run out of cards")
+    (is (= "Player 2" (play-game (first (partition 26 cards)) (last (partition 26 cards)))))
+  )
